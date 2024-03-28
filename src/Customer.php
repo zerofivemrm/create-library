4c2f78d38e96a4f88f3b274586add3d8aca8d928
Muchammad Rifki Maulana <mrifkimaulana1998@gmail.com>
2024-03-28 23:56:47 +0700

    Add default value - Customer.php

--- src/Customer.php
+++ src/Customer.php
@@ -8,7 +8,7 @@ class Customer
   {
   }
 
-  public function sayHello(string $name): string
+  public function sayHello(string $name = "{Guest}"): string
   {
     return "Halo $name, nama saya $this->name";
   }
